39ca066b79f1bf1712b969b1e1c46f4458629f67
Henio Junior <henioajunior@gmail.com>
2021-08-11 11:18:15 -0300

    Lambdas e funções anônimas com parâmetros e retorno

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,18 +1,19 @@
 package br.com.alura.bytebank
 
 fun main() {
-    testaTipoFuncaoReferencia()
-    testaFuncaoTipoClasse()
+//    testaTipoFuncaoReferencia()
+//    testaFuncaoTipoClasse()
 
-//    val minhaFuncaoLambda = {
-//        println("Executa como lambda")
-//    }
-//    println(minhaFuncaoLambda())
-//    println()
-//    val minhaFuncaoAnonima: () -> Unit = fun(){
-//        println("Executa como anonima")
-//    }
-//    println(minhaFuncaoAnonima())
+    val minhaFuncaoLambda: (Int, Int) -> Int =  {
+        a, _ -> a + 1
+    }
+    println(minhaFuncaoLambda(15, 10))
+
+    println()
+    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a: Int, b: Int) : Int {
+        return a + b
+    }
+    println(minhaFuncaoAnonima(10,10))
 }
 
 fun testaFuncaoTipoClasse() {
